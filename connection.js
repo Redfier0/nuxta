e28450227710f03d29dcaa57d65b77b02105d957
Alessandro La Rosa <alinolarosa@gmail.com>
2022-10-06 18:23:18 +0200

    database setup

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: './.env' });
 const mysql = require("mysql")
 
 const con = mysql.createPool({
-    host: "localhost",
+    host: process.env.DATABASE_URL,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME
